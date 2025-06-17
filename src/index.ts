a04211fee665fc33cc389d69c16f09e9bbe8d490
spacegt <541980655@qq.com>
2025-06-17 18:32:28 +0800

    增加组织选择按钮

--- src/index.ts
+++ src/index.ts
@@ -2,33 +2,24 @@
 import type { App } from "vue";
 
 import login from "@/components/login/index.vue";
-
 import aippt from "@/components/aippt/index.vue";
-
 import myaccount from "@/components/myaccount/index.vue";
-
 import snackbar from "@/components/Snackbar.vue";
-
 import manageRoles from "@/components/manage/Roles.vue";
-
 import manageOrgs from "@/components/manage/Orgs.vue";
-
 import manageUsers from "@/components/manage/Users.vue";
+import selectionOrgs from "@/components/extend/SelectionOrgs.vue";
+import selectionOrgsBtn from "@/components/extend/OrgsBtn.vue";
+import batchUsers from "@/components/extend/BatchUsers.vue";
 
 import { useAccountsStore } from "@/stores/accounts";
-
 import { useAuthorizationStore } from "@/stores/authorization";
-
 import { useLoginStore } from "@/stores/login";
-
 import { useSnackbarStore } from "@/stores/snackbar";
 
 import accountsService from "@/axios/accounts-service";
-
 import { config as accountsServiceConfig } from "@/axios/accounts-service";
-
 import emailService from "@/axios/email-service";
-
 import { config as emailServiceConfig } from "@/axios/email-service";
 
 import en from "@/locales/en";
@@ -36,18 +27,15 @@ import zhHans from "@/locales/zhHans";
 
 const install = (app: App) => {
   app.component("spacegt-login", login);
-
   app.component("spacegt-aippt", aippt);
-
   app.component("spacegt-myaccount", myaccount);
-
   app.component("spacegt-snackbar", snackbar);
-
   app.component("spacegt-manage-roles", manageRoles);
-
   app.component("spacegt-manage-orgs", manageOrgs);
-
   app.component("spacegt-manage-users", manageUsers);
+  app.component("spacegt-selection-orgs", selectionOrgs);
+  app.component("spacegt-selection-orgs-btn", selectionOrgsBtn);
+  app.component("spacegt-batch-users", batchUsers);
 };
 
 export default {
@@ -75,5 +63,8 @@ declare module "vue" {
     manageRoles: typeof manageRoles;
     manageOrgs: typeof manageOrgs;
     manageUsers: typeof manageUsers;
+    selectionOrgs: typeof selectionOrgs;
+    selectionOrgsBtn: typeof selectionOrgsBtn;
+    batchUsers: typeof batchUsers;
   }
 }
